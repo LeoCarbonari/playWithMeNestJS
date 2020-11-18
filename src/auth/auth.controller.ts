59a3e7e33f5da4b9147ca926dfcbe1d850746227
Leonardo Carbonari Dela Torre <carbonarileo@hotmail.com>
2020-11-18 15:03:14 -0300

    Introducing JWT operations
    Following on the NestJS lessons, here is the set up and some beginner use of JWTs to ensure safety on sign in operations.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,6 +10,11 @@ export class AuthController {
 
     @Post('/signup')
     signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
-        return this.authService.signUp(authCredentialsDto)
+        return this.authService.signUp(authCredentialsDto);
+    }
+
+    @Post('/signin')
+    signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
+        return this.authService.signIn(authCredentialsDto);
     }
 }

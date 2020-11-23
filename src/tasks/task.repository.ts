16d3865fedabc65be2b5cb5ab8f91be5b92ef8c9
Leonardo Carbonari Dela Torre <carbonarileo@hotmail.com>
2020-11-23 14:25:38 -0300

    Adding logs
    Introducing logs to some of the application methods. Experiencing with different kind of logs.

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -4,9 +4,12 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from "./task-status.enum";
 import { GetTaskFilterDto } from "./dto/get-tasks-filter.dto";
 import { User } from '../auth/user.entity';
+import { InternalServerErrorException, Logger } from "@nestjs/common";
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+    private logger = new Logger("TaskRepository");
+
     async getTasks(
         filterDto: GetTaskFilterDto,
         user: User,
@@ -23,8 +26,14 @@ export class TaskRepository extends Repository<Task> {
             query.andWhere("(task.title LIKE :search OR task.description LIKE :search)", { search: `%${search}%`});
         }
 
-        const tasks = await query.getMany();
-        return tasks;
+        try {
+            const tasks = await query.getMany();
+            return tasks;
+        } catch (error) {
+            this.logger.error(`Failed to get tasks for user "${user.username}". Filters: ${JSON.stringify(filterDto)}`, error.stack);
+            throw new InternalServerErrorException();
+        }
+        
     }
 
     async createTask(
@@ -38,9 +47,14 @@ export class TaskRepository extends Repository<Task> {
         task.description = description;
         task.status = TaskStatus.OPEN;
         task.user = user;
-        await task.save();
-        delete task.user;
-
-        return task;
+        try {
+            await task.save();
+            delete task.user;
+    
+            return task;
+        } catch (error) {
+            this.logger.error(`Failed to create task for user "${user.username}". Data: ${JSON.stringify(createTaskDto)}`, error.stack);
+            throw new InternalServerErrorException();
+        }
     }
 }
